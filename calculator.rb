f1fb4d07f1dfc535764290f3dd9cc2e730ce45e8
Lennie <lennie.mbotela@student.moringaschool.com>
2023-06-26 00:07:49 +0300

    step one and two done

---
+++ calculator.rb
@@ -0,0 +1,21 @@
+ puts "enter a number..."
+ num1 = gets.chomp().to_i
+ 
+ puts "enter another number..."
+ num2 = gets.chomp().to_i
+
+  def addition (num1, num2)
+    return num1+num2
+  end
+
+  def subtraction (num1, num2)
+    return num1-num2
+  end
+
+  def multiplication (num1, num2)
+    return num1*num2
+  end
+  
+  def division (num1, num2)
+    return num1/num2
+  end
