3cf3aec213c4204d27e6f062a23e1fd381100144
Lennie <lennie.mbotela@student.moringaschool.com>
2023-06-26 12:56:46 +0300

    completed

--- calculator.rb
+++ calculator.rb
@@ -1,10 +1,15 @@
+ puts = "CALCULATOR"
+
+  puts = "Fill the first number."
+  num1 = gets.chomp.to_i
+
+  puts = "Fill in the second number."
+  num2 = gets.chomp.to_i
+
   sym = gets.chomp
 
-  num1 = gets.to_i
-  num2 = gets.to_i
-
   def addition(num1, num2)
-    return num1+num2
+    return num1 + num2
   end
 
   add = addition(num1, num2)
@@ -13,7 +18,7 @@
     puts add
 
   def subtraction(num1, num2)
-    return num1-num2
+    return num1 - num2
   end
 
   sub = subtraction(num1, num2)
@@ -22,16 +27,16 @@
     puts sub
 
   def multiplication(num1, num2)
-    return num1*num2
+    return num1 * num2
   end
 
   mult = multiplication(num1, num2)
 
-  elsif sym == "*"
+  elsif sym == "*"          
     puts mult
 
   def division(num1, num2)
-    return num1/num2
+    return num1 / num2
   end
 
   div = division(num1, num2)
